100abe0c75e6be43e8ca63733d662ae7e7cb0af0
Elizaveta Makhortykh <elizamakh@gmail.com>
2019-03-27 12:38:25 +0300

    Corrected mistakes
    Made pages fit to layout, checked styles etc.

--- js/script-file-main.js
+++ js/script-file-main.js
@@ -1,9 +1,6 @@
 var message = document.querySelector(".contact-us");
 var popup_message = document.querySelector(".popup-client-message");
 var close = popup_message.querySelector(".close-window");
-var form = popup_message.querySelector("form");
-var name = popup_message.querySelector("[name=client-name]");
-var email = popup_message.querySelector("[name=client-email]");
 var map = document.querySelector(".show-map");
 var map_full = document.querySelector(".full-map");
 var map_close = map_full.querySelector(".close-window");
@@ -17,13 +14,6 @@ message.addEventListener("click", function (evt) {
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup_message.classList.remove("popup-client-message-show");
-});
-
-form.addEventListener("submit", function (evt) {
-  if (!name.value || !email.value) {
-      evt.preventDefault();
-      console.log("Введите имя и электронный адрес");
-    }
 });
 
 window.addEventListener("keydown", function (evt) {
@@ -46,8 +36,8 @@ map_close.addEventListener("click", function (evt) {
 });
 
 window.addEventListener("keydown", function (evt) {
-  evt.preventDefault();
   if (evt.keyCode === 27) {
+    evt.preventDefault();
     if (map_full.classList.contains("popup-client-message-show")) {
       map_full.classList.remove("popup-client-message-show");
     }
